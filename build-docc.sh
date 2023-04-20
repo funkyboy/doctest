2064ff00acd6e5ce8f1301e685e46e9c7b8a2c6b
Cesare Rocchi <cesarerocchi@gmail.com>
2023-04-20 22:42:04 +0200

    hello

--- build-docc.sh
+++ build-docc.sh
@@ -8,4 +8,4 @@ xcrun xcodebuild docbuild \
 xcrun docc process-archive transform-for-static-hosting \
 	"$PWD/.derivedData/Build/Products/Debug-iphonesimulator/GivenWithLove.doccarchive" \
 	--output-path ".docs" \
-	--hosting-base-path "given-with-love" # required for GitHub Pages, not needed when deploying to Netlify, for example.
+	--hosting-base-path "doctest" # required for GitHub Pages, not needed when deploying to Netlify, for example.
